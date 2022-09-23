8f05c0ea1416b0d7b90834c370f356a5d64c7d5f
Mihray <2636853047@qq.com>
2022-09-23 09:06:24 +0800

    Create Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1> hello</h1>
+    <el-button>提交</el-button>
+    <el-button type="info">提交</el-button>
+    <el-button type="primary">提交</el-button>
+    <el-button type="danger">提交</el-button>
+    <el-button type="success">提交</el-button>
+    <i class="fa fa-users"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
