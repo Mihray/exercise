edc17436f1ea9f615cbeb86d707f412687ec22f0
Mihray <2636853047@qq.com>
2022-10-12 21:19:18 +0800

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Home from '../components/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            redirect:'/login',
+            component:()=>import('@/component/LoginVue.vue')//路由的配置
+        },
+        {
+            path:'/login',
+            name:'LoginVue',
+            component:()=>import('@/component/LoginVue.vue')//路由的配置
+        },
+        {
+            path:'/home',
+            //component:Home
+            component:()=>import('@/component/Home.vue')//路由的配置
+        }
+    ],
+    mode:'history'
+})
