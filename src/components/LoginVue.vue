2e18bf7120958b0a7ec8aebef85a1d33f7f99d2a
Mihray <2636853047@qq.com>
2022-10-12 22:14:17 +0800

    Update LoginVue.vue

--- src/components/LoginVue.vue
+++ src/components/LoginVue.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="login">
         <el-card class="box-card">
-            <div slot="header" class="clearfix">
+            <!-- slot="header" -->
+            <div  slot="header" class="clearfix">
                 <span>通用后台管理系统</span>
             </div>
-        </el-card>
         <el-form  label-width="80px" :model="form" ref="form">
             <el-form-item label="用户名" prop="username">
                 <el-input v-model="form.username"></el-input>
@@ -12,10 +12,11 @@
             <el-form-item label="密码"   prop="password">
                 <el-input type="password" v-model="form.password"></el-input>
             </el-form-item>
-            <el-form-item label="活动形式">
+            <el-form-item >
                 <el-button type="primary">登录</el-button>
             </el-form-item>
         </el-form>
+    </el-card>
     </div>
 </template>
 <script>
@@ -31,5 +32,21 @@ export default {
 }
 </script>
 <style scoped>
-
+.login{
+    width:100%;
+    height:100%;
+    position:absolute;
+    background:#409EFF;
+}
+.box-card{
+        width:450px;
+        margin:200px auto;
+    }
+    
+/* .el-card__haeder{
+        font-size:34px;
+    }
+    .clearfix sapn{
+        font-size:34px;
+    } */
 </style>
