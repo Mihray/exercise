0282bd0e2715abc8163d41121cbbd9aa43800603
Mihray <2636853047@qq.com>
2022-10-14 23:34:26 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // devServer:{
-  //   open:true
-  // }
-})
+module.exports = {
+  devServer: {
+      proxy: {
+          '/mtapi/mihray/login': { // '/api'是代理标识，用于告诉node，url前面是/api的就是使用代理的
+              target: "http://111.229.176.245:3000", //目标地址，一般是指后台服务器地址
+              changeOrigin: true, //是否跨域
+              
+          }
+      }
+  }
+}
