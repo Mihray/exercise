c50b4bcd2f27c93504b58b0c75ebb681f5f1e7f7
Mihray <2636853047@qq.com>
2022-10-18 16:43:29 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import '../plugins/element.js'
 import Element from 'element-ui'
 import 'font-awesome/css/font-awesome.min.css'
 import axios from 'axios'
-// import router from './router'
+import router from './router'
 
 Vue.prototype.axios=axios //挂载到原型，可以全局使用
 Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
