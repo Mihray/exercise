69991bcf9d6fabffd98bc7d9c718f6ce46c2b0df
Mihray <2636853047@qq.com>
2022-10-18 16:43:32 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/Home.vue'
+// import Home from '../components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes:[
         {
             path:'/',
             redirect:'/login',
-            component:()=>import('@/component/LoginVue.vue')//路由的配置
+            component:()=>import('../components/LoginVue.vue')//路由的配置
         },
         {
             path:'/login',
             name:'LoginVue',
-            component:()=>import('@/component/LoginVue.vue')//路由的配置
+            component:()=>import('../components/LoginVue.vue')//路由的配置
         },
         {
             path:'/Home',
             name:'Home',
             // component:Home
-            component:()=>import('@/component/Home.vue')//路由的配置
+            component:()=>import('../components/Home.vue')//路由的配置
         }
     ],
     mode:'history'
 })
+export default router
